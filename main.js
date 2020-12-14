f2eec5bcc39840301d9151287ac0da22fe4d2736
MilanovicNikola <nikolamil83@gmail.com>
2020-12-14 02:00:39 +0100

    finished landing page desktop

--- main.js
+++ main.js
@@ -2,39 +2,30 @@
 'use strict'
 
 
- let time = 4000;
- let i = 1;
- let cards = document.getElementsByClassName('card');
+let time = 4000;
+let i = 1;
+let cards = document.getElementsByClassName('card');
 
 function changeCard(){
   
-  
     if(i > cards.length-1){
-
       i= 1;
-    }if(i < cards.length){
-      cards[i].classList.toggle('visible');
-    
-      setTimeout(changeCard, time);
-  
-      i++;
-      
-      
-      
     }
-    
-  } 
+    if(i < cards.length){
+      cards[i].classList.toggle('visible');
+      setTimeout(changeCard, time);
+      i++;
+    }
+  };
     
   
 
-  function removeClass(){
+  
+  
+   
+ 
+  
 
-      
-      cards[i].classList.remove('visible');
-          
-    };
-  
-  
   
     
   
